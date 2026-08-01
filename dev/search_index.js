4c03f48557fce53df91a9fb6a3e940bf542d14ff
Documenter.jl <documenter@juliadocs.github.io>
2026-08-01 12:06:23 +0000

    build based on 3b94b59

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"category":"section","location":"#BoussinesqApprox","page":"Home","text":"Documentation for BoussinesqApprox.\n\n","title":"BoussinesqApprox"}]
+}
