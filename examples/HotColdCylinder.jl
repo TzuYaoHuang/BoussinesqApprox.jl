5660e677b1b07f28bad26fc686c534f71e60e65e
Tzu-Yao Huang <tzuyao.jason.huang@gmail.com>
2026-08-02 00:28:12 +0200

    Include the more advanced and interesting test cases

---
+++ examples/HotColdCylinder.jl
@@ -0,0 +1,27 @@
+using WaterLily, BoussinesqApprox
+using Plots
+
+function hot_cold_circle(N;Ra=10^6,ε=0.001,Pr=100,mem=Array,T=Float32)
+    NN = (N,N)
+    radius = N÷8
+    body = AutoBody((x,t)->√sum(abs2, x .- (N÷2,N÷4)) - radius) + AutoBody((x,t)->√sum(abs2, x .- (N÷2,3N÷4)) - radius)
+
+    ΔT = T(1)
+    α = ε/ΔT |> T
+    g = T(1)
+
+    νκ = g*α*ΔT*radius^3/Ra
+    ν = sqrt(νκ*Pr)
+    κ = sqrt(νκ/Pr)
+
+    Uf = sqrt(g*α*ΔT*radius)
+    U = Uf/√Pr |> T
+
+    Simulation(NN, (0,0), radius; U, Δt=0.05, ν, κ, flow_ctor=ThermalFlow, g=(i,x,t)-> i==2 ? -g : zero(g), perdir=(1,),θ0=nothing,θb=(x)-> -sign(x[2]-N÷2)*ΔT, body)
+end
+
+sim = hot_cold_circle(128)
+
+# duration=8,step=0.08 covers plume formation through the steady rising plume reached around tU/L≈5
+sim_gif!(sim; duration=40, step=0.08, field=sim->sim.flow.θ, clims=(-1,1), cfill=:seismic,
+              plotbody=true, fname=joinpath(@__DIR__,"HotColdCylinder.gif"))
