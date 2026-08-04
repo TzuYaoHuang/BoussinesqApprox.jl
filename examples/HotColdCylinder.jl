ca30b01f78c818084a0668c74e2d1aedfdeb1151
Tzu-Yao Huang <tzuyao.jason.huang@gmail.com>
2026-08-04 18:46:14 +0200

    Update examples

--- examples/HotColdCylinder.jl
+++ examples/HotColdCylinder.jl
@@ -4,7 +4,7 @@ using Plots
 function hot_cold_circle(N;Ra=10^6,ε=0.001,Pr=100,mem=Array,T=Float32)
     NN = (N,N)
     radius = N÷8
-    body = AutoBody((x,t)->√sum(abs2, x .- (N÷2,N÷4)) - radius) + AutoBody((x,t)->√sum(abs2, x .- (N÷2,3N÷4)) - radius)
+    body = AutoBody((x,t)->√sum(abs2, x .- (N÷3,N÷4)) - radius) + AutoBody((x,t)->√sum(abs2, x .- (2N÷3,3N÷4)) - radius)
 
     ΔT = T(1)
     α = ε/ΔT |> T
@@ -23,5 +23,11 @@ end
 sim = hot_cold_circle(128)
 
 # duration=8,step=0.08 covers plume formation through the steady rising plume reached around tU/L≈5
+
+# Visualize the temperature
 sim_gif!(sim; duration=40, step=0.08, field=sim->sim.flow.θ, clims=(-1,1), cfill=:seismic,
-              plotbody=true, fname=joinpath(@__DIR__,"HotColdCylinder.gif"))
+              plotbody=true, fname=joinpath(@__DIR__,"HotColdCylinder_temp.gif"))
+
+# Visualize the vorticity
+# sim_gif!(sim; duration=40, step=0.08, clims=(-30,30), cfill=:seismic,
+#               plotbody=true, fname=joinpath(@__DIR__,"HotColdCylinder_vort.gif"))
