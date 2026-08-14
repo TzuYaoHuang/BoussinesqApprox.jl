5c875e49306dce26fb6e6ccb75422788873b81ec
Tzu-Yao Huang (Asus) <tzuyao.jason.huang@gmail.com>
2026-08-14 14:49:26 +0200

    Cooler example

--- examples/HotColdCylinder.jl
+++ examples/HotColdCylinder.jl
@@ -2,9 +2,9 @@ using WaterLily, BoussinesqApprox
 using Plots
 
 function hot_cold_circle(N;Ra=10^6,ε=0.001,Pr=100,mem=Array,T=Float32)
-    NN = (N,N)
+    NN = (N,2N)
     radius = N÷8
-    body = AutoBody((x,t)->√sum(abs2, x .- (N÷3,N÷4)) - radius) + AutoBody((x,t)->√sum(abs2, x .- (2N÷3,3N÷4)) - radius)
+    body = AutoBody((x,t)->√sum(abs2, x .- (N÷2,7N÷5)) - radius) + AutoBody((x,t)->√sum(abs2, x .- (N÷2,3N÷5)) - radius)
 
     ΔT = T(1)
     α = ε/ΔT |> T
@@ -17,16 +17,16 @@ function hot_cold_circle(N;Ra=10^6,ε=0.001,Pr=100,mem=Array,T=Float32)
     Uf = sqrt(g*α*ΔT*radius)
     U = Uf/√Pr |> T
 
-    ThermalSimulation(NN, (0,0), radius; U, Δt=0.05, ν, α, κ, g=(i,x,t)-> i==2 ? -g : zero(g), perdir=(1,),θ0=nothing,θb=(x,t)-> -sign(x[2]-N÷2)*ΔT, body)
+    ThermalSimulation(NN, (0,0), radius; U, Δt=0.05, ν, α, κ, g=(i,x,t)-> i==2 ? -g : zero(g), perdir=(1,),θ0=nothing,θb=(x,t)-> -sign(x[2]-N)*ΔT, body)
 end
 
 # using CUDA
-sim = hot_cold_circle(128)#; mem=CuArray)
+sim = hot_cold_circle(96)#; mem=CuArray)
 
 # duration=8,step=0.08 covers plume formation through the steady rising plume reached around tU/L≈5
 
 # Visualize the temperature
-sim_gif!(sim; duration=20, step=0.08, f=(dat,sim)->copyto!(dat,sim.flow.θ), clims=(-1,1), cfill=:seismic,
+sim_gif!(sim; duration=20, step=0.08, f=(dat,sim)->copyto!(dat,sim.flow.θ), clims=(-1,1), cfill=:seismic, hidedecorations=true,
               plotbody=true, video=joinpath(@__DIR__,"HotColdCylinder_temp.gif"))
 
 # Visualize the vorticity
